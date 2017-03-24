61369aa3706088301baeb892aa9cbc8e1e927bb2
Justin Thomas <jet_5060@yahoo.com>
2017-03-24 17:03:17 -0400

    Move beer type field from Package -> Batch
    A Batch can made of only one Beer and each package has to be
    associated with a batch.

--- beer/models.py
+++ beer/models.py
@@ -1,32 +1,33 @@
 from django.db import models
 
 class Beer( models.Model ):
-    name = models.CharField( max_length=256 )
+    name = models.CharField( max_length = 256 )
 
     def __str__( self ):
         return self.name
 
 class Batch( models.Model ):
     date = models.DateField()
+    beer = models.ForeignKey( Beer,
+                              related_name = 'batches' )
 
 class PackageType( models.Model ):
     capacity = models.FloatField()
-    name = models.CharField( max_length=256 )
+    name = models.CharField( max_length = 256 )
 
     def __str__( self ):
         return self.name
 
 class Package( models.Model ):
-    batch = models.ForeignKey( Batch )
+    batch = models.ForeignKey( Batch,
+                               related_name = 'packages' )
     package_type = models.ForeignKey( PackageType )
-    beer = models.ForeignKey( Beer,
-                              related_name='packages')
 
     def __str__( self ):
-        return str( self.package_type ) + ' ' + str( self.beer )
+        return str( self.package_type ) + ' ' + str( self.batch.beer )
 
 class Customer( models.Model ):
-    name = models.CharField( max_length=256 )
+    name = models.CharField( max_length = 256 )
 
     def __str__( self ):
         return self.name
@@ -36,10 +37,10 @@ class Order( models.Model ):
     date_promised = models.DateField()
     date_filled = models.DateField()
     customer = models.ForeignKey( Customer,
-                                  related_name='orders' )
-    packages = models.ForeignKey( Package, 
-                                  related_name='orders' )
+                                  related_name = 'orders' )
+    packages = models.ForeignKey( Package,
+                                  related_name = 'orders' )
 
     def __str__( self ):
-        return self.customer + ' ' + len(self.packages) + 'Items'
+        return self.customer + ' ' + len( self.packages ) + 'Items'
 
