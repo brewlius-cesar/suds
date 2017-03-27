2ca2973e280f2ebb64ef213054d36845207164e6
Justin Thomas <jet_5060@yahoo.com>
2017-03-27 15:22:35 -0400

    Build-out Order model to have OrderLineItems
    Needed a way to distniguish between the package requirements of an
    order and what packages were used to meet those requirements.
    
    With this we can:
      - Distinguish between filled, and un-filled orders
      - Know what is needed to fill an order
      - Plan batches based on what is required to fill orders
      - Know exactly what packages/batches went into a given order
        (for later QC issues?)

--- beer/models.py
+++ beer/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.db import models
 
 class Beer( models.Model ):
@@ -22,6 +24,10 @@ class Package( models.Model ):
     batch = models.ForeignKey( Batch,
                                related_name = 'packages' )
     package_type = models.ForeignKey( PackageType )
+    line_item = models.ForeignKey( 'OrderLineItem',
+                                   related_name = 'packages',
+                                   null = True,
+                                   blank = True )
 
     def __str__( self ):
         return str( self.package_type ) + ' ' + str( self.batch.beer )
@@ -32,15 +38,30 @@ class Customer( models.Model ):
     def __str__( self ):
         return self.name
 
+class OrderLineItem( models.Model ):
+    package_type = models.ForeignKey( PackageType,
+                                      related_name = 'line_items' )
+    quantity = models.PositiveIntegerField()
+    order = models.ForeignKey( 'Order',
+                               related_name = 'line_items' )
+    beer = models.ForeignKey( 'Beer',
+                              related_name = 'line_items' )
+
 class Order( models.Model ):
-    date_placed = models.DateField()
+    date_placed = models.DateField( default = date.today )
     date_promised = models.DateField()
-    date_filled = models.DateField()
+    date_filled = models.DateField( null = True,
+                                    blank = True )
     customer = models.ForeignKey( Customer,
                                   related_name = 'orders' )
-    packages = models.ForeignKey( Package,
-                                  related_name = 'orders' )
-
     def __str__( self ):
         return self.customer + ' ' + len( self.packages ) + 'Items'
+
+    def ItemCount( self ):
+        tot_items = 0
+        for line_item in self.line_items:
+            tot_items += line_item.quantity
+
+
+        return tot_items
 
